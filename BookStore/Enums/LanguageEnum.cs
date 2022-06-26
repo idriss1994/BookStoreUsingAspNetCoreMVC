6bd9b26b1dce79c27df0661b702face34c558355
ait si el arabi <driss9452@hotmail.fr>
2022-06-26 14:30:26 +0000

    Add LanguageEnum

---
+++ BookStore/Enums/LanguageEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Enums
+{
+    public enum LanguageEnum
+    {
+        Arabic,
+        Amazigh,
+        English,
+        French,
+        Hindi,
+        Dutch
+    }
+}
