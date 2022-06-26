fd351deaba5de8db7c0536ff2eeb468a625b93f5
ait si el arabi <driss9452@hotmail.fr>
2022-06-26 14:46:14 +0000

    Applay DataAnnotation to enum

--- BookStore/Enums/LanguageEnum.cs
+++ BookStore/Enums/LanguageEnum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,22 @@ namespace BookStore.Enums
 {
     public enum LanguageEnum
     {
-        Arabic,
-        Amazigh,
-        English,
-        French,
-        Hindi,
-        Dutch
+        [Display(Name = "Arabic language")]
+        Arabic = 1,
+
+        [Display(Name = "Amazigh language")]
+        Amazigh = 2,
+
+        [Display(Name = "English language")]
+        English = 3,
+
+        [Display(Name = "French language")]
+        French = 4,
+
+        [Display(Name = "Hindi language")]
+        Hindi = 5,
+
+        [Display(Name = "Dutch language")]
+        Dutch = 6
     }
 }
