28df18d44dbb08cb16fafda6e1c29269616d29b3
ait si el arabi <driss9452@hotmail.fr>
2022-06-26 13:51:55 +0000

    Fill select tag helper using SelectListItem class

--- BookStore/Controllers/BookController.cs
+++ BookStore/Controllers/BookController.cs
@@ -37,10 +37,14 @@ namespace BookStore.Controllers
         [HttpGet]
         public IActionResult AddNewBook(int id = 0, bool isSuccess = false)
         {
+            var model = new BookModel
+            {
+                Language = "2" // binding the value attribute of option tag in select tag.
+            };
             ViewBag.BookId = id;
             ViewBag.IsSuccess = isSuccess;
-            ViewBag.Languages = GetLanguages();
-            return View();
+            ViewBag.Languages = GetLanguagesUsingSelectListItem();
+            return View(model);
         }
 
         [HttpPost]
@@ -58,12 +62,35 @@ namespace BookStore.Controllers
             ViewBag.ModelNotValid = true;
             ModelState.AddModelError("", "This my custome error msg1");
             ModelState.AddModelError("", "This my custome error msg2");
-            ViewBag.Languages = GetLanguages();
+            ViewBag.Languages = GetLanguagesUsingSelectListItem();
             return View();
         }
-        private SelectList GetLanguages()
+        private IEnumerable<SelectListItem> GetLanguages()
         {
-            return new SelectList(new List<string> { "Arabic", "English", "French" });
+            var list = new List<LanguageModel>
+            {
+                new LanguageModel { Id = 1, Text = "Arabic" },
+                new LanguageModel { Id = 2, Text = "English" },
+                new LanguageModel { Id = 3, Text = "French" }
+            };
+
+            return list.Select(l => new SelectListItem
+            {
+                Text = l.Text,
+                Value = l.Id.ToString()
+            });
+         }
+        private List<SelectListItem> GetLanguagesUsingSelectListItem()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Arabic", Value = "1" },
+                new SelectListItem { Text = "English", Value = "2" },
+                new SelectListItem { Text = "Amazigh", Value = "3" },
+                new SelectListItem { Text = "French", Value = "4" },
+                new SelectListItem { Text = "Hindi", Value = "5" },
+                new SelectListItem { Text = "Dutch", Value = "6" },
+            };
         }
     }
 }
