e670f7d68d9b6638f796df0ec7b2abc405aeaac0
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-07-01 00:57:50 +0100

    Add Language Model

---
+++ BookStore/Data/Language.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BookStore.Data
+{
+    public class Language
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //create relation between Language and Books (One to Many)
+        public ICollection<Book> Books { get; set; }
+    }
+}
