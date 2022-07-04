dde7708b79ec7f2023218a40a5b8be665b4c9a47
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-07-04 22:26:42 +0100

    Add custom Vaalidation Attribute

---
+++ BookStore/Helpers/MyCustomValidationAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Helpers
+{
+    public class MyCustomValidationAttribute : ValidationAttribute
+    {
+        string _text;
+        public MyCustomValidationAttribute(string text)
+        {
+            _text = text;
+        }
+        public string Text { get; set; }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value != null)
+            {
+                string bookName = value as string;
+                if (bookName.Contains(_text))
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            return new ValidationResult(ErrorMessage ?? $"The name of the book does not contain  the value {_text}!");
+        }
+    }
+}
