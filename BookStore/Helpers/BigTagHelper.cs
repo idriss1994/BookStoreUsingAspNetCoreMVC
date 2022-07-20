1c94605730f784eaaaff383569ce48eb4f4e95e4
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-07-20 13:25:51 +0100

    working with custom tag helpers

---
+++ BookStore/Helpers/BigTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BookStore.Helpers
+{
+    [HtmlTargetElement("big")]
+    [HtmlTargetElement(Attributes = "big")]
+    public class BigTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "h3";
+            output.Attributes.RemoveAll("big");
+            output.Attributes.SetAttribute("class", "text-danger");
+            output.PostContent.SetHtmlContent(" idriss");
+            output.PostContent.SetHtmlContent(" ait si el arabi");
+        }
+    }
+}
---
+++ BookStore/Helpers/CustomEmailTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BookStore.Helpers
+{
+    public class CustomEmailTagHelper : TagHelper
+    {
+        //dynamic attribute for tag helper by creating a property member
+
+        [HtmlAttributeName("asp-my-email")]
+        public string MyEmail { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", $"{MyEmail}");
+            output.Attributes.Add(new TagHelperAttribute("id", "my-email-id"));
+            output.Attributes.Add("name", "email");
+            output.Content.SetContent("my-email");
+        }
+    }
+}
