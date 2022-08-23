4c60e75c992d5768ad5c2329ab68b5248b82f0a3
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-08-23 18:19:04 +0100

    create view component and invoke it in index view

---
+++ BookStore/Components/TopBooksViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System.Threading.Tasks;
+
+namespace BookStore.Components
+{
+    public class TopBooksViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
