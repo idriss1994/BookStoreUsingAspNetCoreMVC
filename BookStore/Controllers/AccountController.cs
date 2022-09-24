49cde33487a251d6f860569f563db6a2fe37a7c8
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-09-24 22:15:57 +0100

    Start implementation of sign up fonctionnality

---
+++ BookStore/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("signup")]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("signup")]
+        public IActionResult Signup(SignUpUserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
---
+++ BookStore/Models/SignUpUserModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class SignUpUserModel
+    {
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a strong password")]
+        [Compare("ConfirmPassword", ErrorMessage = "Password does not match")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
